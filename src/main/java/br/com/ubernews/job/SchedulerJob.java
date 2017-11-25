f9fdebf6d5ca3a084e02e7e2e475143a7fa6ac20
JONAS <jonasgoulart@developerworks.com.br>
2017-11-25 00:30:59 -0200

    Add project to GitHub

---
+++ src/main/java/br/com/ubernews/job/SchedulerJob.java
@@ -0,0 +1,12 @@
+package br.com.ubernews.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class SchedulerJob implements Job {
+	
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		
+	}
+}
